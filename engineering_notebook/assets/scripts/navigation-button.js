8a0f61e99f03c6b1162bad76474c13e47e85d5e0
timathon <timathon@outlook.com>
2025-12-18 06:23:49 +0800

    ch06: added WRC世界机器人大赛北京选拔赛

--- engineering_notebook/assets/scripts/navigation-button.js
+++ engineering_notebook/assets/scripts/navigation-button.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
     linksContainer.classList.add('navigation-links');
     menu.appendChild(linksContainer);
 
-                                                    const chapterPageCounts = {
+                                                        const chapterPageCounts = {
         "index": 4,
         "glossary": 0,
         "ch01": 27,
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
         "ch03": 10,
         "ch04": 10,
         "ch05": 10,
-        "ch06": 11,
+        "ch06": 15,
         "ch07": 12,
         "ch08": 8
     };
